88e43fae2649edf621b62fdc331fbabe438c4529
Rodrigo Funes Biasatti <rodrigo11.funes@hotmail.com>
2022-08-30 20:17:01 -0300

    REFACTOR: reestructuracion de carpetas

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,12 @@
+import "./Expenses.css"
+import ExpenseItem from "./ExpenseItem";
+import Card from "../GeneralComponents/Card";
+
+function Expenses (props) {
+    const expenses = props.expenses;
+    return (
+        <Card className="expenses">{expenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}</Card>
+    );
+};
+
+export default Expenses;
