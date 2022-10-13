49d4a4e6b237dd548f7ce93c4dbc089af967bd06
Rodrigo Funes Biasatti <rodrigo11.funes@hotmail.com>
2022-10-13 17:15:50 -0300

    ADD: seccion 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,24 @@
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
 import Card from "../GeneralComponents/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('');
     const expenses = props.expenses;
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
-        <Card className="expenses">{expenses.map(expense => <ExpenseItem key={props.id} title={expense.title} amount={expense.amount} date={expense.date} />)}</Card>
+        <div>    
+            <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {expenses.map(expense => <ExpenseItem key={props.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+            </Card>
+        </div>
     );
 };
 
